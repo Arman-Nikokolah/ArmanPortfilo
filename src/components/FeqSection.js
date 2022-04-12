9710a04b8ffcc5dd41fee9448c5acb93db1c3443
Arman-Nikokolah <arman.nikokolah@gmail.com>
2022-04-12 09:44:24 +0430

    FaqSection

---
+++ src/components/FeqSection.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const FeqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Question <span>FAQ</span>
+      </h2>
+      {/* 1 */}
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. In,
+            cupiditate.
+          </p>
+        </div>
+      </div>
+      {/* 2 */}
+      <div className="question">
+        <h4>Daily Schedules?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. In,
+            cupiditate.
+          </p>
+        </div>
+      </div>
+      {/* 3 */}
+      <div className="question">
+        <h4>Diffrent Pyment Methods? </h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. In,
+            cupiditate.
+          </p>
+        </div>
+      </div>
+      {/* 4 */}
+      <div className="question">
+        <h4>What Product Do you Offer ?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. In,
+            cupiditate.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
-import React, { Fragment } from "react";
+import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FeqSection from "../components/FeqSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FeqSection />
     </>
   );
 };
